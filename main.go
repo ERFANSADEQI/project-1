775e15debebb80d0470aea733c054d62eb58ba3b
ERFAN <sadeqierfan00@gmail.com>
2024-05-09 23:12:36 +0330

    Second commit

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func main() {
+	router := gin.Default()
 
+	router.POST("/echo", func(c *gin.Context) {
+		var message map[string]interface{}
+
+		if err := c.BindJSON(&message); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(200, gin.H{"message": message})
+	})
+
+	router.Run(":8080")
+}
