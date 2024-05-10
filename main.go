e01070ae622a4d3a8a1edf900c2791ff71402267
ERFAN <sadeqierfan00@gmail.com>
2024-05-10 19:18:26 +0330

    edit commit

--- main.go
+++ main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
 )
+
+var wsupgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool{
+		return true
+	},
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+    if err!= nil {
+       http.NotFound(w, r)
+	   return
+	}
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err!= nil {
+            break
+        }
+
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+}
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.POST("/echo", func(c *gin.Context) {
-		var message map[string]interface{}
-
-		if err := c.BindJSON(&message); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.JSON(200, gin.H{"message": message})
-	})
+	
+    router.GET("/ws/echo", func(c *gin.Context) {
+        wshandler(c.Writer, c.Request)
+    })
 
 	router.SetTrustedProxies([]string{"127.0.0.1"})
-	router.Run(":8889")
+    router.Run(":8889")
 }
